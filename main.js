6649bc0a02e25e7495d24cbe854886896534c676
Damian <dsimrayh@yahoo.com>
2021-12-15 19:08:20 -0800

    Functionality added except for read/remove

--- main.js
+++ main.js
@@ -1,33 +1,91 @@
-let library = [];
+let myLibrary = [];
 
-// Boiler plate constructor =====================
-
-function Book(title, author, pages, read) {
+function Book(title, author, pages) {
 	this.title = title;
  	this.author = author;
   	this.pages = pages;
-  	this.read = read;
-  	this.info = function() {
-  		return `${title} by ${author}, ${pages} pages, ${read ? 'read' : 'not yet read'}`;
-  	}
+	this.read = false;
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-const LOTR = new Book("Lord of the Rings", "J.R.R. Tolkien", 300, true);
-
-console.log(theHobbit.info());
-console.log(LOTR.info());
-
-// ==================================
-
-
+// Return values are used to allow/prevent displayBooks function from being called
 function addBookToLibrary() {
-	// Call when user sunmits new book form
+	const bookTitle = document.querySelector("#title").value;
+	const bookAuthor = document.querySelector("#author").value;
+	const bookPages = document.querySelector("#pages").value;
+
+	if(!bookTitle || !bookAuthor || !bookPages) return false;
+
+	const newBook = new Book(bookTitle, bookAuthor, bookPages)
+	myLibrary.push(newBook);
+	const formMask = document.querySelector("#mask");
+	formMask.classList.add('hidden');
+	resetForm();
+
+	return true;
+}
+
+function resetForm() {
+	document.querySelector("#title").value = null;
+	document.querySelector("#author").value = null;
+	document.querySelector("#pages").value = null;
+}
+
+// Creates card to display with book info
+function createBaseCard(book) {
+	const card = document.createElement("div");
+	card.classList.add("card");
+
+	const title = document.createElement("p");
+	title.classList.add("book-title");
+	title.innerText = book.title;
+
+	const author = document.createElement("p");
+	author.classList.add("book-author");
+	author.innerText = book.author;
+
+	const pages = document.createElement("p");
+	pages.classList.add("book-pages");
+	pages.innerText = book.pages
+
+	card.appendChild(title);
+	card.appendChild(author);
+	card.appendChild(pages);
+
+	return card;
+}
+
+// Creates "Read" and "Remove" for cards
+function createCardOptions() {
+	const options = document.createElement("div");
+	options.classList.add("book-options");
+
+	const label = document.createElement("label");
+	label.classList.add("book-read");
+	label.innerText = "Read";
+
+	const input = document.createElement("input");
+	input.setAttribute("type", "checkbox")
+	input.classList.add("book-read-checkbox");
+
+	const button = document.createElement("button");
+	button.classList.add("remove-book");
+	button.innerText = "Remove";
+
+	options.appendChild(label);
+	options.appendChild(input);
+	options.appendChild(button);
+
+	return options;
 }
 
 function displayBooks() {
-	// Loop through array and display books
-	// Called once on load, called subsequently on each add / remove
+	const library = document.querySelector("#library");
+	myLibrary.forEach(book => {
+		const card = createBaseCard(book);
+		const options = createCardOptions()
+		card.appendChild(options);
+		library.appendChild(card);
+	});
 }
 
 function addAllButtonListeners () {
@@ -37,16 +95,19 @@ function addAllButtonListeners () {
 	const closeNewBookForm = document.querySelector("#close-form");
 	const formMask = document.querySelector("#mask");
 
-	newBookButton.addEventListener('click', () => {
-		formMask.classList.toggle('hidden');
+	newBookButton.addEventListener("click", () => {
+		formMask.classList.toggle("hidden");
 	});
 
-	closeNewBookForm.addEventListener('click', () => {
-		formMask.classList.toggle('hidden');
+	closeNewBookForm.addEventListener("click", () => {
+		formMask.classList.toggle("hidden");
 	});
 
-	submitNewBook.addEventListener('click', (e) => {
+	submitNewBook.addEventListener("click", (e) => {
 		e.preventDefault();
+		const bookAdded = addBookToLibrary();
+		if(bookAdded) displayBooks();
+		else return;
 	})
 }
 
